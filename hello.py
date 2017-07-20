fbbd69b2f9e553cfa5a0fc1ec08d060c2b6c2e72
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2017-07-20 11:46:18 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
+from __future__ import print_function
 import os
 import uuid
-from flask import Flask
+import urlparse
+import json
+import socket
+from flask import Flask, render_template, jsonify
 
+from cfenv import AppEnv
+
+env = AppEnv()
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -10,19 +17,23 @@ GREEN = "#33CC33"
 
 COLOR = GREEN
 
+counterK = 0
+
 @app.route('/')
+
 def hello():
-
-
-    return """
+   whoamiK = socket.gethostname()
+   return """
     <html>
     <body bgcolor="{}">
     <center><h1><font color="white">Hi, I'm GUID:<br/>
-    {}</br>
-    </center>
+	{}
+	<center><h1><font color="white">My hostname is:<br/>
+    {}
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,whoamiK)
+	
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0')
+	 app.run(debug=False,host='0.0.0.0', port=env.port)
