4618248a0fc1e3fc628802edb40475f3fd732766
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2017-07-20 11:37:18 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,53 +1,28 @@
-from __future__ import print_function
 import os
 import uuid
-from flask import Flask, render_template
-from cfenv import AppEnv
-import time
-from pprint import pformat
+from flask import Flask
 
 
-env = AppEnv()
 app = Flask(__name__)
+my_uuid = str(uuid.uuid1())
+BLUE = "#0099FF"
+GREEN = "#33CC33"
 
-start = time.time()
-
-from flask import request
-
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+COLOR = GREEN
 
 @app.route('/')
 def hello():
-    now = time.time()
-    uptime = now-start
-    m, s = divmod(uptime, 60)
-    uptime = "%02dm%02ds" % (m, s)
-    return render_template("index.html",index=env.index,uptime=uptime)
 
-@app.route('/exit')
-def exit():
-    now = time.time()
-    uptime = now-start
-    m, s = divmod(uptime, 60)
-    uptime = "%02dm%02ds" % (m, s)
-    shutdown_server()
-    return render_template("exit.html",index=env.index,uptime=uptime)
 
-@app.route('/env')
-def showenv():
-    return render_template("env.html",env=os.environ)
-
-@app.route('/port')
-def port():
-    return env.port
-
-@app.route('/index')
-def index():
-    return env.index
+    return """
+    <html>
+    <body bgcolor="{}">
+    <center><h1><font color="white">Hi, I'm GUID:<br/>
+    {}</br>
+    </center>
+    </body>
+    </html>
+    """.format(COLOR,my_uuid,)
 
 if __name__ == "__main__":
-app.run(debug=False,host='0.0.0.0',port=int(os.getenv('PORT', '80'))
+app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '80')))
